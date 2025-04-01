37e5e8d6494647d66c07ee4e2dd436532b4f52a9
Mik Wiseman <hi@mikwiseman.com>
2025-04-01 22:46:46 +0300

    Article added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -199,20 +199,20 @@
         </router-link>
 
         <!-- Second Blog Post -->
-        <router-link :to="{ name: $i18n.locale === 'ru' ? 'blog-post-ru' : 'blog-post', params: { id: 'ai-trends' }}" class="project-card blog-card">
+        <router-link :to="{ name: $i18n.locale === 'ru' ? 'blog-post-ru' : 'blog-post', params: { id: 'germes-presentation' }}" class="project-card blog-card">
           <div class="blog-preview">
-            <img src="@/assets/images/poisk-klientov.jpg" loading="lazy" alt="AI Trends" class="blog-preview-image">
+            <img src="@/assets/images/germes-presentation.jpg" loading="lazy" alt="AI Sales Presentation" class="blog-preview-image">
           </div>
           <div class="blog-content">
             <div class="project-info">
               <div class="project-title">
-                <div class="proj-title">{{ $t('blog.post2.title') }}</div>
-                <div class="proj-description">{{ $t('blog.post2.description') }}</div>
+                <div class="proj-title">{{ $t('blog.post2.title', 'Мик Вайсман выступит с докладом об AI в продажах в бизнес-клубе Germes') }}</div>
+                <div class="proj-description">{{ $t('blog.post2.description', '17 апреля при поддержке Московской Торгово-Промышленной палаты состоится встреча бизнес-клуба Germes. Тема мероприятия: «Цифровая трансформация российского бизнеса: ключевые вызовы, успехи и перспективы».') }}</div>
               </div>
               <div class="project-chips-container">
-                <div class="chip on-color dark">AI Trends</div>
-                <div class="chip on-color dark">Technology</div>
-                <div class="chip on-color dark">Future</div>
+                <div class="chip on-color dark">Events</div>
+                <div class="chip on-color dark">AI Sales</div>
+                <div class="chip on-color dark">Business</div>
               </div>
             </div>
           </div>
