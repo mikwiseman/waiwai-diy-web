30f00de24c6c30c87f42acc7ddeb0b0730f7188f
Mik Wiseman <hi@mikwiseman.com>
2025-04-23 17:12:19 +0300

    Added english version

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,17 +5,15 @@ import en from '@/translations/en.json'
 // Function to determine locale based on domain
 function getLocaleFromDomain() {
   const domain = window.location.hostname
-  console.log('Detected domain:', domain); // Added for debugging
-  if (domain === 'wai.sales' || domain === 'wai.sale') {
-    console.log('Setting locale to: en'); // Added for debugging
+  
+  if (domain.includes('wai.sale')) {
     return 'en'
   }
-  console.log('Setting locale to: ru'); // Added for debugging
-  return 'ru' // Default to Russian for waiwai.diy and other domains
+  return 'ru'
 }
 
 export default createI18n({
-  legacy: false, // Set to false to use Composition API
+  legacy: false, 
   locale: getLocaleFromDomain(),
   fallbackLocale: 'ru',
   messages: {
