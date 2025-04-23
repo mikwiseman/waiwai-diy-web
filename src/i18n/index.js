ccd4758c048d0894447d2534e1268b5829d745e7
Mik Wiseman <hi@mikwiseman.com>
2025-04-23 17:03:48 +0300

    Added english version

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,22 @@
 import { createI18n } from 'vue-i18n'
 import ru from '@/translations/ru.json'
+import en from '@/translations/en.json'
+
+// Function to determine locale based on domain
+function getLocaleFromDomain() {
+  const domain = window.location.hostname
+  if (domain === 'wai.sales') {
+    return 'en'
+  }
+  return 'ru' // Default to Russian for waiwai.diy and other domains
+}
 
 export default createI18n({
   legacy: false, // Set to false to use Composition API
-  locale: 'ru',
+  locale: getLocaleFromDomain(),
   fallbackLocale: 'ru',
   messages: {
-    ru
+    ru,
+    en
   }
 }) 
