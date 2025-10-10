613c1c763c1637356d7daa822ccb91491d00d784
Mik Wiseman <hi@mikwiseman.com>
2025-10-10 09:56:09 +0300

    Tg link added

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,33 @@ const routes = [
     component: Home
   },
   {
+    path: '/mikwiseman',
+    name: 'telegram-redirect',
+    beforeEnter: () => {
+      const telegramUrl = 'https://t.me/mikwiseman'
+      if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+        let redirected = false
+        const redirect = () => {
+          if (!redirected) {
+            redirected = true
+            window.location.href = telegramUrl
+          }
+        }
+
+        window.gtag('event', 'qr_link_visit', {
+          event_category: 'qr_redirect',
+          event_label: 'mikwiseman',
+          transport_type: 'beacon',
+          event_callback: redirect
+        })
+
+        setTimeout(redirect, 500)
+      } else {
+        window.location.href = telegramUrl
+      }
+    }
+  },
+  {
     path: '/blog',
     name: 'blog',
     component: Blog
